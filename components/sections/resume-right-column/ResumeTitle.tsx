044cf3a0aa360248a0057a2632bf3b4dd7b5a3d8
ninohdasilva <ninohesteban@gmail.com>
2024-04-11 23:49:18 +0200

    change ResumeTitle font weight

--- components/sections/resume-right-column/ResumeTitle.tsx
+++ components/sections/resume-right-column/ResumeTitle.tsx
@@ -5,8 +5,8 @@ export default function ResumeTitle() {
     const t = useTranslations('Data')
     return (
         <div className="resume-title text-center rounded-xl ring-1 ring-foreground p-2">
-            <h1 className="text-5xl">{t("resume")} {t("title")}</h1>
-            <h2>Resume subtitle</h2>
+            <h1 className="text-5xl font-extrabold">{t("resume")} {t("title")}</h1>
+            <h2 className="font-bold">Resume subtitle</h2>
         </div>
     )
 }
